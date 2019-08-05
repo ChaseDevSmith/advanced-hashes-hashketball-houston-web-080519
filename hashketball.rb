aa233594f7835012129e92989c33669df30e89e5
Chase Deveraux Smith <chases832@gmail.com>
2019-08-05 12:01:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,6 +121,7 @@ def num_points_scored(players_name)
     team.each do |attribute, data|
       if attribute == :players 
         data.each  do |player|
+          if player[:players_name] == playe
       binding.pry 
     end 
     end 
